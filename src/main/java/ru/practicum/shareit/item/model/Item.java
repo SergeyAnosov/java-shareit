8536427835a6205a94a6c03663f5071596f71989
Sergey Anosov <106556264+SergeyAnosov@users.noreply.github.com>
2023-01-10 08:52:49 +0300

    Update Item.java

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,23 +4,12 @@ import lombok.Data;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Data
 public class Item {
-    private long id;
-    @NotBlank(message = "Имя не может быть пустым")
-    private String name;
-
-    @NotNull
-    @Size(max = 200)
-    private String description;
-
-    @NotNull
-    private Boolean available;
-    @NotNull
+    private long id;    
+    private String name;   
+    private String description;   
+    private Boolean available;    
     private User owner;
     private ItemRequest request;
 
